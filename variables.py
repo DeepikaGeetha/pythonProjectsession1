97062f2d17cebbcf709d0a13e323c3047b93ca6a
Deepikageetha <dee@gmail.com>
2022-04-27 18:52:02 +0530

    Initial commit sess1

---
+++ variables.py
@@ -0,0 +1,14 @@
+print("my first pyhton code")
+
+#variables ->store data in memory
+name="sam"
+
+a=10
+b=25.5
+is_valid_user=True
+#to know the datatypes of above variables
+
+print(type(name))
+print(type(a))
+print(type(b))
+print(type(is_valid_user))
